a237684614e7bf0146308cc8430066f06444b7a2
Victormrf <vmrf2000@hotmail.com>
2024-10-21 03:05:48 -0300

    Initialized project and configures Users and Tours API

---
+++ routes/tourRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { 
+    checkID,
+    checkBody,
+    getAllTours, 
+    createTour, 
+    getTour, 
+    updateTour, 
+    deleteTour 
+} = require('../controllers/tourController');
+
+router.param('id', checkID);
+
+router
+    .route('/')
+    .get(getAllTours)
+    .post(checkBody, createTour);
+
+router
+    .route('/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour);
+
+module.exports = router;
