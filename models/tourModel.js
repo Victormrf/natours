10fb860c8d6e1338f9d914e14d79885e63bb5ad4
Victormrf <vmrf2000@hotmail.com>
2024-10-25 03:39:44 -0300

    Started API improvement

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,47 @@ const tourSchema = new mongoose.Schema({
 		required: [true, 'A tour must have a name'],
 		unique: true
 	},
-	rating: {
+	maxGroupSize: {
 		type: Number,
-		default: 4.5 // If we dont specify a rating, it will be automatically set to 4.5
+		required: [true, 'A tour must have a group size'] 
+	},
+	difficulty: {
+		type: String,
+		required: [true, 'A tour must have a difficulty'] 
+	},
+	ratingsAverage: {
+		type: Number,
+		required: 4.5 
+	},
+	ratingsQuantity: {
+		type: Number,
+		required: 0
 	},
 	price: {
 		type: Number,
 		required: [true, 'A tour must have a price']
 	},
+	priceDiscount: Number,
+	summary: {
+		type: String,
+		trim: true,
+		required: [true, "A tour must have a summary"]
+	},
+	description: {
+		type: String,
+		trim: true,
+	},
+	imageCover: {
+		type: String,
+		required: [true, "A tour must have a cover image"]
+	},
+	images: [String],
+	createdAt: {
+		type: Date,
+		default: Date.now()
+	},
+	startDates: [Date]
+
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
