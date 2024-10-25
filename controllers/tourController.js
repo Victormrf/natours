d207c8a3b87438dca1475669e42b1e401c9a5b88
Victormrf <vmrf2000@hotmail.com>
2024-10-25 03:39:44 -0300

    Started API improvement

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,8 +3,36 @@ const Tour = require('./../models/tourModel');
 // Gets all tours 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        // Build query
+        // 1A) Filtering ---------------------------------------
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
 
+        // 1B) Advanced Filtering ------------------------------
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        // const query = Tour.find(queryObj);  // (1A)
+        let query = Tour.find(JSON.parse(queryStr));  // (1B)
+
+        // 2) Sorting ------------------------------------------
+        if(req.query.sort){
+            query = query.sort(req.query.sort);
+        } else{ // Adding a default sort based on date of creation
+            query = query.sort('-createdAt');
+        }
+
+        // Execute query
+        const tours = await query;
+
+        // const tours = await Tour.find()
+        //     .where('duration')
+        //     .lte(5)
+        //     .where('difficulty')
+        //     .equals('easy');
+
+        // Send response
         res.status(200).json({
             status: 'success',
             data: {
