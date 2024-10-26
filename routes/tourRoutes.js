72fbed70d029d656604b2d0fd11193a9dd0a63bc
Victormrf <vmrf2000@hotmail.com>
2024-10-26 03:36:25 -0300

    Implemented Aggregation Pipelines

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,16 +4,22 @@ const {
     /*checkID,*/
     /*checkBody,*/
     aliasTopTours,
+    getTourStats,
     getAllTours,
     createTour,
     getTour,
     updateTour,
-    deleteTour
+    deleteTour,
+    getMonthlyPlan
 } = require('./../controllers/tourController');
 
 // router.param('id', checkID);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
+
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router
     .route('/')
