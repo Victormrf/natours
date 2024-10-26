cbf4ace439f016554ee7da24fcc70b67c42acf33
Victormrf <vmrf2000@hotmail.com>
2024-10-26 03:36:25 -0300

    Implemented Aggregation Pipelines

--- models/tourModel.js
+++ models/tourModel.js
@@ -46,8 +46,16 @@ const tourSchema = new mongoose.Schema({
 		default: Date.now()
 	},
 	startDates: [Date]
-
+}, {
+	toJSON: { virtuals: true },
+	toObject: { virtuals: true }
 });
+
+tourSchema.virtual('durationWeeks').get(function() {
+	return this.duration / 7;
+});
+
+tourSchema.pre
 
 const Tour = mongoose.model('Tour', tourSchema);
 
