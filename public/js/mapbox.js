b2bd18c5bda705dacc41023e8cf4755dbc413a1c
Victormrf <vmrf2000@hotmail.com>
2024-11-23 04:23:16 -0300

    Implemented tour page

---
+++ public/js/mapbox.js
@@ -0,0 +1,2 @@
+console.log('Helo from the client side!');
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).render('base', {
-        tour: 'The Forest Hiker',
-        user: 'Victor'
-    });
-});
-
-router.get('/overview',viewsController.getOverview);
-router.get('/tour', viewsController.getTour);
+router.get('/',viewsController.getOverview);
+router.get('/tour/:slug', viewsController.getTour);
 
 module.exports = router;
 
