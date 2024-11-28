3c2ae04249ad110d8ca7efe7ffb7aa2c838967f2
Victormrf <vmrf2000@hotmail.com>
2024-11-28 03:24:03 -0300

    Finished implementing user settings interface and functionalities

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use('/api', limiter); //affects all APIs that start with this url
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
