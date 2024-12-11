9856346d8633c20e6a2e3cf8ad02c9b5e605e1a4
Victormrf <vmrf2000@hotmail.com>
2024-12-10 22:06:42 -0300

    Prepared app for deployment

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -76,6 +77,8 @@ app.use(hpp({
         'price'
     ]
 }));
+
+app.use(compression())
 
 // Test middlewares
 app.use((req, res, next) => {
