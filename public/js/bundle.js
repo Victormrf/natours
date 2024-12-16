00c29e9e2a48ac05e2afab134460423c1df06c8b
Victormrf <vmrf2000@hotmail.com>
2024-12-16 01:45:42 -0300

    Last commit before rendering application on render

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,2 +1,164 @@
-(()=>{var d=e=>{mapboxgl.accessToken="pk.eyJ1IjoidmljdG9ybXJmOCIsImEiOiJjbTRkcnBiNWgwMzBzMmtwejA5b2owOWdyIn0.zLL2t5utzZJzlOTcfNuL8Q";var t=new mapboxgl.Map({container:"map",style:"mapbox://styles/mapbox/light-v10",scrollZoom:!1});let o=new mapboxgl.LngLatBounds;e.forEach(a=>{let n=document.createElement("div");n.className="marker",new mapboxgl.Marker({element:n,anchor:"bottom"}).setLngLat(a.coordinates).addTo(t),new mapboxgl.Popup({offset:30}).setLngLat(a.coordinates).setHTML(`<p>Day ${a.day}: ${a.description}</p>`).addTo(t),o.extend(a.coordinates)}),t.fitBounds(o,{padding:{top:200,bottom:150,left:100,right:100}})};var c=()=>{let e=document.querySelector(".alert");e&&e.parentElement.removeChild(e)},s=(e,t)=>{c();let o=`<div class="alert alert--${e}">${t}</div>`;document.querySelector("body").insertAdjacentHTML("afterbegin",o),window.setTimeout(c,5e3)};var i=async(e,t)=>{try{(await axios({method:"POST",url:"/api/v1/users/login",data:{email:e,password:t}})).data.status==="success"&&(s("success","Logged in successfully"),window.setTimeout(()=>{location.assign("/")},1500))}catch(o){s("error",o.response.data.message)}},u=async()=>{try{(await axios({method:"GET",url:"/api/v1/users/logout"})).data.status==="success"&&location.reload(!0)}catch{s("error","Error logging out! Try again")}};var r=async(e,t)=>{try{let o=t==="password"?"/api/v1/users/updateMyPassword":"/api/v1/users/updateMe";(await axios({method:"PATCH",url:o,data:e})).data.status==="success"&&s("success",`${t.toUpperCase()} updated successfully!`)}catch(o){s("error",o.response.data.message)}};var m=async e=>{try{let t=Stripe("pk_test_51QSuWUDuHI9lwyv3UCS5WnJTos77QaKSKTqOv3j97NzvU579iHtfTa2DuZG8AJfO6U3dLrl2XyAIp9Td9UgRtNu600Wy2g29BL"),o=await axios(`/api/v1/bookings/checkout-session/${e}`);await t.redirectToCheckout({sessionId:o.data.session.id})}catch(t){console.log(t),s("error",t)}};var l=document.getElementById("map"),p=document.querySelector(".form--login"),g=document.querySelector(".nav__el--logout"),y=document.querySelector(".form-user-data"),w=document.querySelector(".form-user-password"),f=document.getElementById("book-tour");if(l){let e=JSON.parse(l.dataset.locations);d(e)}p&&p.addEventListener("submit",e=>{e.preventDefault();let t=document.getElementById("email").value,o=document.getElementById("password").value;i(t,o)});g&&g.addEventListener("click",u);y&&y.addEventListener("submit",e=>{e.preventDefault();let t=new FormData;t.append("name",document.getElementById("name").value),t.append("email",document.getElementById("email").value),t.append("photo",document.getElementById("photo").files[0]),r(t,"data")});w&&w.addEventListener("submit",async e=>{e.preventDefault(),document.querySelector(".btn--save-password").textContent="Updating...";let t=document.getElementById("password-current").value,o=document.getElementById("password").value,a=document.getElementById("password-confirm").value;await r({passwordCurrent:t,password:o,passwordConfirm:a},"password"),document.querySelector(".btn--save-password").textContent="Save password",document.getElementById("password-current").value="",document.getElementById("password").value="",document.getElementById("password-confirm").value=""});f&&f.addEventListener("click",e=>{e.target.textContent="Processing...";let{tourId:t}=e.target.dataset;m(t)});})();
-//# sourceMappingURL=bundle.js.map
+(() => {
+  // public/js/mapbox.js
+  var displayMap = (locations) => {
+    mapboxgl.accessToken = "pk.eyJ1IjoidmljdG9ybXJmOCIsImEiOiJjbTRkcnBiNWgwMzBzMmtwejA5b2owOWdyIn0.zLL2t5utzZJzlOTcfNuL8Q";
+    var map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/light-v10",
+      scrollZoom: false
+    });
+    const bounds = new mapboxgl.LngLatBounds();
+    locations.forEach((loc) => {
+      const el = document.createElement("div");
+      el.className = "marker";
+      new mapboxgl.Marker({
+        element: el,
+        anchor: "bottom"
+      }).setLngLat(loc.coordinates).addTo(map);
+      new mapboxgl.Popup({
+        offset: 30
+      }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`).addTo(map);
+      bounds.extend(loc.coordinates);
+    });
+    map.fitBounds(bounds, {
+      padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100
+      }
+    });
+  };
+
+  // public/js/alerts.js
+  var hideAlert = () => {
+    const el = document.querySelector(".alert");
+    if (el) el.parentElement.removeChild(el);
+  };
+  var showAlert = (type, msg) => {
+    hideAlert();
+    const markup = `<div class="alert alert--${type}">${msg}</div>`;
+    document.querySelector("body").insertAdjacentHTML("afterbegin", markup);
+    window.setTimeout(hideAlert, 5e3);
+  };
+
+  // public/js/login.js
+  var login = async (email, password) => {
+    try {
+      const res = await axios({
+        method: "POST",
+        url: "/api/v1/users/login",
+        data: {
+          email,
+          password
+        }
+      });
+      if (res.data.status === "success") {
+        showAlert("success", "Logged in successfully");
+        window.setTimeout(() => {
+          location.assign("/");
+        }, 1500);
+      }
+    } catch (err) {
+      showAlert("error", err.response.data.message);
+    }
+  };
+  var logout = async () => {
+    try {
+      const res = await axios({
+        method: "GET",
+        url: "/api/v1/users/logout"
+      });
+      if (res.data.status === "success") {
+        location.reload(true);
+      }
+    } catch (err) {
+      showAlert("error", "Error logging out! Try again");
+    }
+  };
+
+  // public/js/updateSettings.js
+  var updateSettings = async (data, type) => {
+    try {
+      const url = type === "password" ? "/api/v1/users/updateMyPassword" : "/api/v1/users/updateMe";
+      const res = await axios({
+        method: "PATCH",
+        url,
+        data
+      });
+      if (res.data.status === "success") {
+        showAlert("success", `${type.toUpperCase()} updated successfully!`);
+      }
+    } catch (err) {
+      showAlert("error", err.response.data.message);
+    }
+  };
+
+  // public/js/stripe.js
+  var bookTour = async (tourId) => {
+    try {
+      const stripe = Stripe("pk_test_51QSuWUDuHI9lwyv3UCS5WnJTos77QaKSKTqOv3j97NzvU579iHtfTa2DuZG8AJfO6U3dLrl2XyAIp9Td9UgRtNu600Wy2g29BL");
+      const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+      await stripe.redirectToCheckout({
+        sessionId: session.data.session.id
+      });
+    } catch (err) {
+      console.log(err);
+      showAlert("error", err);
+    }
+  };
+
+  // public/js/index.js
+  var mapBox = document.getElementById("map");
+  var loginForm = document.querySelector(".form--login");
+  var logOutBtn = document.querySelector(".nav__el--logout");
+  var userDataForm = document.querySelector(".form-user-data");
+  var userPasswordForm = document.querySelector(".form-user-password");
+  var bookBtn = document.getElementById("book-tour");
+  if (mapBox) {
+    const locations = JSON.parse(mapBox.dataset.locations);
+    displayMap(locations);
+  }
+  if (loginForm) {
+    loginForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      login(email, password);
+    });
+  }
+  if (logOutBtn) {
+    logOutBtn.addEventListener("click", logout);
+  }
+  if (userDataForm) {
+    userDataForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const form = new FormData();
+      form.append("name", document.getElementById("name").value);
+      form.append("email", document.getElementById("email").value);
+      form.append("photo", document.getElementById("photo").files[0]);
+      updateSettings(form, "data");
+    });
+  }
+  if (userPasswordForm) {
+    userPasswordForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      document.querySelector(".btn--save-password").textContent = "Updating...";
+      const passwordCurrent = document.getElementById("password-current").value;
+      const password = document.getElementById("password").value;
+      const passwordConfirm = document.getElementById("password-confirm").value;
+      await updateSettings({ passwordCurrent, password, passwordConfirm }, "password");
+      document.querySelector(".btn--save-password").textContent = "Save password";
+      document.getElementById("password-current").value = "";
+      document.getElementById("password").value = "";
+      document.getElementById("password-confirm").value = "";
+    });
+  }
+  if (bookBtn) {
+    bookBtn.addEventListener("click", (e) => {
+      e.target.textContent = "Processing...";
+      const { tourId } = e.target.dataset;
+      bookTour(tourId);
+    });
+  }
+})();
